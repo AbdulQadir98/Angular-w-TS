ee0809d68da20a91be34693c64b6207a1a3b00eb
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-09-10 17:52:54 +0530

    add notes

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { __values } from 'tslib';
 import { Todo } from './../../models/Todo';
 
 @Component({
@@ -8,22 +9,45 @@ import { Todo } from './../../models/Todo';
 })
 export class TodosComponent implements OnInit {
 
-  title:string = 'Title';
+  title:string = 'my list';
   todos:Todo[];
+
+  inputNote:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
     this.todos = [
-      {
-        content: "First todo",
-        completed: false
-      },
-      {
-        content: "Second todo",
-        completed: true
-      }
+      // {
+      //   content: "First todo",
+      //   completed: false
+      // },
+      // {
+      //   content: "Second todo",
+      //   completed: false
+      // }
     ]
+  }
+
+  // loop thorugh all todos and check if matches the id
+  toggleDone(id:Number) {
+    this.todos.map((value, index) => {
+      if (index == id) value.completed = !value.completed;
+      return value;
+    })
+  }
+
+  deleteTodo(id:Number) {
+    this.todos = this.todos.filter((value, index) => index !== id)
+  }
+
+  addTodo () {
+    this.todos.push({
+      content : this.inputNote,
+      completed : false
+    });
+
+    this.inputNote = "";
   }
 
 }
